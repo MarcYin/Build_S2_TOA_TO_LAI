78da3113d118ee5fc35e1c59b0332a7a81a842ab
MarcYin <marcollinbobo@gmail.com>
2019-01-29 22:36:14 +0000

    add license

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,9 +1,8 @@
 conda install conda-build  
 conda install anaconda-client 
-PKG_NAME=s2-toa-to-lai && USER=f0xy  
+USER=f0xy  
 conda config --set anaconda_upload no  
 conda build . -c conda-forge -c f0xy
 export CONDA_PACKAGE=`conda build --output . -c conda-forge -c f0xy | grep bz2`
 echo $CONDA_PACKAGE
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_PACKAGE --force
-
