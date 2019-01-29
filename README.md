9953fe33ee111f39c6b9dc68544be9634119a08c
MarcYin <marcollinbobo@gmail.com>
2019-01-29 17:51:08 +0000

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+#Build conda package for S2_TOA_TO_LAI
+### Feng Yin
+### Department of Geography, UCL
+### ucfafyi@ucl.ac.uk
+
+
---
+++ conda_upload.sh
@@ -0,0 +1,9 @@
+conda install conda-build  
+conda install anaconda-client 
+PKG_NAME=s2-toa-to-lai && USER=f0xy  
+conda config --set anaconda_upload no  
+conda build . -c conda-forge -c f0xy
+export CONDA_PACKAGE=`conda build --output . | grep bz2`
+echo $CONDA_PACKAGE
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_PACKAGE --force
+
