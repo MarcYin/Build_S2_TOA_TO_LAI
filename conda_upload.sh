2eb647d322acc6fb1525fc48a10eb4e664824f65
MarcYin <marcollinbobo@gmail.com>
2019-01-29 18:25:21 +0000

    add siac

--- conda_upload.sh
+++ conda_upload.sh
@@ -3,7 +3,7 @@ conda install anaconda-client
 PKG_NAME=s2-toa-to-lai && USER=f0xy  
 conda config --set anaconda_upload no  
 conda build . -c conda-forge -c f0xy
-export CONDA_PACKAGE=`conda build --output . | grep bz2`
+export CONDA_PACKAGE=`conda build --output . -c conda-forge -c f0xy | grep bz2`
 echo $CONDA_PACKAGE
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_PACKAGE --force
 
